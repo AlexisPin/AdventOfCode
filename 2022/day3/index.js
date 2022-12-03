6eb7ed8f2bbf7f9fe540a4315e03392e4e8965e6
AlexisPin <pinceminalexis@gmail.com>
2022-12-03 11:52:05 +0100

    day 3

--- 2022/day3/index.js
+++ 2022/day3/index.js
@@ -1,3 +1,4 @@
+//Part 1
 l = $('*').innerText.split`\n`
 	.map((x) => [x.slice(0, x.length / 2), x.slice(x.length / 2)])
 	.filter((x) => x[0].length > 1)
@@ -6,6 +7,7 @@ l = $('*').innerText.split`\n`
 		return a + x.charCodeAt(0) - (x.toUpperCase() === x ? 39 : 97) + 1;
 	}, 0);
 
+//Part 2
 l = $('*').innerText.split`\n`
 	.filter((x) => x.length > 1)
 	.reduce((a, c, i, arr) => {
